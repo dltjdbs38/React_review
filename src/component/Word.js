e8110314d13183955097e06ee695971710baacf0
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-19 16:34:47 +0900

    fetch DELETE만 하면 새로고침 눌러야 화면에 반영이 됨. 따라서 word.id를 0으로 만들어 word.id가 0이면 null을 반환

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+  const [word, setWord] = useState(w);
+  //{word:w}로 받고 w쓰는 거랑 props 로 받고 props.word랑 같음.
   const [isShow, setIsShow] = useState(false);
   //isShow && word.kor ... isShow가 true일떄만 AND문으로 인해 보여진다.
   const [isDone, setIsDone] = useState(word.isDone);
@@ -32,8 +34,16 @@ export default function Word({ word }) {
     if (window.confirm("삭제 하시겠습니까?")) {
       fetch(`http://localhost:3001/words/${word.id}`, {
         method: "DELETE",
+      }).then((res) => {
+        if (res.ok) {
+          setWord({ id: 0 });
+        }
       });
     }
+  }
+  if (word.id === 0) {
+    //삭제 버튼을 누르면 fetch DELETE가 일어나고 word의 id를 0으로 만들었었음. 그러므로 word.id===0이면 null을 리턴해서 아무것도 띄우지 않게 한다.
+    return null;
   }
   return (
     <tr className={isDone ? "off" : ""}>
