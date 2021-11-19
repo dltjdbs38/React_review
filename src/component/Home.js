0e31655ac7b2980af93689a02a98c0e4395204e1
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-19 15:46:32 +0900

    엥 커밋 꼬인듯.. useEffect 해서 3001번 포트에서 days 받아오기

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,14 @@ export default function Home() {
   const [days, setDays] = useState([]);
 
   useEffect(() => {
-    console.log("Count Change!");
+    fetch("http://localhost:3001/days")
+      .then((res) => {
+        //res는 http응답이고 실제 json은 아니다 그래서 .json()
+        return res.json();
+      }) //다음 then은 이전 then의 return 값을 res로 받아온다..?
+      .then((data) => {
+        setDays(data);
+      });
   }, []); //useEffect는 페이지에서 무언가 랜더링이 되면 실행됨. 따라서 특정 변수가 변경됐을 떄만 실행하고 싶다면 ,다음 인자를 []안에 넣어주면 딘다.(의존성 배열)
   //의존성 배열과 상관없이 만약 새로고침 최초에 한번만 실행하고 싶다면 ,뒤에 빈 배열 []을 넣어주면 된다.
   return (
