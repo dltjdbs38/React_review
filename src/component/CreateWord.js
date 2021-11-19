15694cc9d8bf4d6b851b2290c1ed556e186d0de1
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-19 16:58:02 +0900

    단어를 만들 day를 선택할 옵션을 data.json에서 가져오는 기능 구현

--- src/component/CreateWord.js
+++ src/component/CreateWord.js
@@ -1,4 +1,6 @@
+import useFetch from "../hooks/useFetch";
 export default function CreateWord() {
+  const days = useFetch("http://localhost:3001/days");
   return (
     <form>
       <div className="input_area">
@@ -12,8 +14,9 @@ export default function CreateWord() {
       <div className="input_area">
         <label>Day</label>
         <select>
-          <option>1</option>
-          <option>2</option>
+          {days.map((day) => {
+            <option key={day.id} value={day.day}></option>; // day를 여기서 선택해서 해당 day에 단어를 create한다.
+          })}
         </select>
       </div>
       <button>저장</button>
