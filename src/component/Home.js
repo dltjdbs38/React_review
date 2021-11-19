abe118896aa8844828eeebfe77fb4bd817269713
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-19 13:28:51 +0900

    useEffect 와 List태그 버튼처럼 꾸미기, 배열 state에 ...배열 이용해서 요소로 객체 값 추가

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,40 @@
 import { Link } from "react-router-dom";
 import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  console.log(dummy);
+  const [days, setDays] = useState([]);
+  const [count, setCount] = useState(0);
+  function onClick() {
+    setCount(count + 1);
+  }
+  function onClick2() {
+    setDays([
+      ...days,
+      {
+        id: Math.random(),
+        day: 1,
+      },
+    ]);
+  }
+
+  useEffect(() => {
+    console.log("Count Change!");
+  }, [count]); //useEffect는 페이지에서 무언가 랜더링이 되면 실행됨. 따라서 특정 변수가 변경됐을 떄만 실행하고 싶다면 ,다음 인자를 []안에 넣어주면 딘다.(의존성 배열)
+  //의존성 배열과 상관없이 만약 새로고침 최초에 한번만 실행하고 싶다면 ,뒤에 빈 배열 []을 넣어주면 된다.
   return (
-    <ul className="list_day">
-      {dummy.days.map((myday) => {
-        return (
-          <li key={myday.id}>
-            <Link to={`/day/${myday.day}`}>Day {myday.day}</Link>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className="list_day">
+        {days.map((myday) => {
+          return (
+            <li key={myday.id}>
+              <Link to={`/day/${myday.day}`}>Day {myday.day}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={onClick}>{count}</button>
+      <button onClick={onClick2}>Day Change</button>
+    </>
   );
 }
