095a4855d9c08f0e269231b5b9218635d1fc8022
Seoyoon-Scalar <aassdd38@naver.com>
2021-11-20 20:07:16 +0900

    Heroku로 배포 시도 server파일 내 index.js만들고 package.json 수정함

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server");
+const { resolve } = require("path");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(__dirname + "/db.json"));
+const middlewares = jsonServer.defaults({
+  static: path.resolve(__dirname + "/../build"),
+});
+
+const port = process.env.PORT || 3001;
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+server.listen(port, () => {
+  console.log("Hey, JSON Server is running!!");
+});
